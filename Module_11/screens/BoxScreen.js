7d5737a10988935a9cf387da62b12271fb97ed30
Pl1sat <arberqeriqi2011@gmail.com>
2024-12-12 20:53:45 +0100

    day_11
    day11

--- Module_11/screens/BoxScreen.js
+++ Module_11/screens/BoxScreen.js
@@ -3,11 +3,14 @@ import { View, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
   return (
-    <View style={{ flex: 1, flexDirection: 'row-reverse' }}>
+    <View style={{ flex: 1, flexDirection: 'column', justifyContent:"space-around" ,
+       
+        alignItems:"strech"
+}}>
       {/* Corrected the color "powerblue" */}
       <View style={{ width: 50, height: 50, backgroundColor: "powderblue" }}></View>
       <View style={{ width: 50, height: 50, backgroundColor: "skyblue" }}></View>
-      <View style={{ width: 50, height: 50, backgroundColor: "red" }}></View>
+      <View style={{  height: 50, backgroundColor: "red" }}></View>
     </View>
   );
 };
