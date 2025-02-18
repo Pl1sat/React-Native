5e367c583a6416eb933df282a5f6c41ad0777ff4
Pl1sat <arberqeriqi2011@gmail.com>
2025-02-18 22:22:50 +0100

    day25
    day25

---
+++ Module26/screens/Login.js
@@ -0,0 +1,74 @@
+
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        try {
+            const response = await fetch("https://dummyjson.com/auth/login", {
+                method: "POST",
+                headers: { 'Content-Type': "application/json" },
+                body: JSON.stringify({
+                    username: username,
+                    password: password
+                }),
+                credentials: "omit"
+            });
+
+            const data = await response.json();
+            console.log(data);
+            if(data?.token){
+                console.log("Next Page")
+            }
+        } catch (err) {
+            console.error("Login failed:", err);
+        }
+    };
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Fake Login</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Username"
+                value={username}
+                autoCapitalize="none"
+                onChangeText={text => setUsername(text)}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="Password"
+                autoCapitalize="none"
+                secureTextEntry={true}
+                value={password}
+                onChangeText={text=> setPassword(text)}
+            />
+            <Button title="Login" onPress={handleLogin} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        paddingHorizontal: 20,
+        backgroundColor: "#fff"
+    },
+    title: {
+        fontSize: 24,
+        marginBottom: 20,
+        textAlign: "center"
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: "#aaa",
+        padding: 10,
+        marginVertical: 10,
+        borderRadius: 5
+    }
+});
+
+export default Login;  
